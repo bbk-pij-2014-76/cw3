a64d6f7af8b3ef0b339812cb30072e7220a40fe8
BBK-PIJ-2014-76 <syed.maqsood.ali@live.com>
2014-11-30 14:13:19 +0000

    MyList.java was added to the project

---
+++ MyList.java
@@ -0,0 +1,33 @@
+public class MyList implements List
+{
+	public boolean isEmpty ()
+	{
+			if (this.isEmpty())
+				return true;
+			else
+				return false;
+	}
+	public int size()
+	{
+		return this.size();
+	}
+
+	public ReturnObject get(int index)
+	{
+		return get(index);
+	}
+
+	public ReturnObject remove(int index)
+	{
+		return remove(index);
+	}
+	public ReturnObject add(int index, Object item)
+	{
+		return add (index,item);
+	}
+	public ReturnObject add(Object item)
+	{
+		return add(item);
+	}
+
+}
