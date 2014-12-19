f6378dfc07e9336ab496953b7c84a882228a0d37
BBK-PIJ-2014-76 <syed.maqsood.ali@live.com>
2014-12-19 10:23:22 +0000

    code was added for all methods in the file along with apppropriate wrapper of return objects

--- ArrayList.java
+++ ArrayList.java
@@ -1,33 +1,79 @@
 public class ArrayList implements List
 {
+	private static int ARRAY_SIZE = 20;
+	private int arraySize;
+	private Object theArray[];
+
+	public ArrayList () {
+		theArray  = new Object [ARRAY_SIZE];
+		arraySize = 0;
+	}
+
+
 	public boolean isEmpty()
 	{
-	    if (this.isEmpty())
+	    if (arraySize == 0)
 			return true;
 		else
 			return false;
 	}
 	public int size()
 	{
-		return this.size();
+		return arraySize;
 	}
 
 	public ReturnObject get(int index)
 	{
-		return get(index);
+		if ( index < 0 || index >= arraySize ) {
+			return new ReturnObjectImpl (ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			return new ReturnObjectImpl(theArray[index]);
+		}
 	}
 
 	public ReturnObject remove(int index)
 	{
-		return remove(index);
+		if ( theArray[index] == null )
+		{
+			return new ReturnObjectImpl (ErrorMessage.EMPTY_STRUCTURE);
+		}
+		else
+		{
+			for ( int i=index ; i < arraySize ; i++ )
+			{
+				theArray[i] = theArray[i+1];
+			}
+			arraySize--;
+			return new ReturnObjectImpl(theArray[index]);
+		}
+
 	}
 	public ReturnObject add(int index, Object item)
 	{
-		return add (index,item);
+		if ( index < 0 || index >= arraySize ) {
+			return new ReturnObjectImpl (ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else if (item == null ) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else {
+			for ( int i=arraySize ; i > index ; i-- ) {
+				theArray [i+1] = theArray [i];
+			}
+			theArray [index] = item;
+			arraySize++;
+			return new ReturnObjectImpl(theArray[index]);
+		}
 	}
+
 	public ReturnObject add(Object item)
 	{
-		return add(item);
+		if ( item == null )
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		else
+		{
+			theArray[arraySize] = item;
+			arraySize++;
+			return new ReturnObjectImpl(theArray[arraySize]);
+		}
 	}
 
 }
