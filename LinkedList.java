dc17def8310722a06f78337ea2e943839460d7e0
BBK-PIJ-2014-76 <syed.maqsood.ali@live.com>
2014-12-20 11:11:04 +0000

    add method was modified along with remove method

--- LinkedList.java
+++ LinkedList.java
@@ -1,52 +1,67 @@
 public class LinkedList implements List
 {
-	private Node head; 		// reference to the head node.
-	private int listCount; 	// just for speed
+	private Node theHead; 		// reference to the head node.
 
 	public LinkedList()
 	{
 		// this is an empty list, so the reference to the head node
 		// is set to a new node with no data
-		head = new NodeImpl(null);
-		listCount = 0;
+		theHead    = null;
 	}
 
 	public boolean isEmpty ()
 	{
-			if (this.isEmpty())
-				return true;
-			else
-				return false;
+		if (theHead == null)
+			return true;
+		else
+			return false;
+	}
+	public int countNodes ( Node node )
+	{
+		if ( node != null )
+			return 1 + countNodes (node.getNext());
+		else
+			return 0;
 	}
 	public int size()
 	{
-
-   		return size();
+		return countNodes(theHead);
 	}
 
 	public ReturnObject get(int index)
 	{
-		return get(index);
+		if (index < 0 || index >= size() )
+			return new ReturnObjectImpl ( ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		else {
+			Node theNode = theHead;
+			for ( int i = 0; i < index ; i++)
+				theNode = theNode.getNext();
+				return  new ReturnObjectImpl (theNode.getData());
+		}
 	}
 
 	public ReturnObject remove(int index)
 	{
-		return remove(index);
+		return new ReturnObjectImpl ( ErrorMessage.INDEX_OUT_OF_BOUNDS);
 	}
-	public ReturnObject add(int index, Object item)
+	public ReturnObject add(Object item)
 	{
-		return add (index,item);
+		return add (item);
 	}
-	public ReturnObject add(int listCount, Object data) {
-		Node temp = new NodeImpl(data);
-		Node current = head;
-		// starting at the head node, crawl to the end of the list
-		while (current.getNext() != null) {
-			current = current.getNext();
+	public ReturnObject add(int index, Object item) {
+		if ( item != null )
+		{
+			if ( index < 0 || index > size() )
+				return new ReturnObjectImpl ( ErrorMessage.INDEX_OUT_OF_BOUNDS);
+			else
+			{
+				if ( theHead == null && index == 0)
+				{	NodeImpl newNode = new NodeImpl(item);
+							theHead  = newNode;
+				}
+			}
+
 		}
-		// the last node's "next" reference set to our new node
-		current.setNext(temp);
-		listCount++;// increment the number of elements variable
-		return current;
+		return new ReturnObjectImpl (theHead);
 	}
 }
